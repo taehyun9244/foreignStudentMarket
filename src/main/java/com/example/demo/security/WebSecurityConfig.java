8ea790978e2c42f9f0e8c83d550a6645089f3c19
taehyun9244 <85472257+taehyun9244@users.noreply.github.com>
2022-03-28 03:40:40 +0900

    User login and test code

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -19,44 +20,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-// h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
+        // h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
         web
                 .ignoring()
                 .antMatchers("/h2-console/**");
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-// 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
-        http.csrf()
-                .ignoringAntMatchers("/user/**");
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy((SessionCreationPolicy.STATELESS))//세션정책, jwt토큰 사용을 위해 STATELESS
+                .and()
+                .antMatcher("/user/**");
 
-        http.authorizeRequests()
-                // image 폴더를 login 없이 허용
-                .antMatchers("/images/**").permitAll()
-                // css 폴더를 login 없이 허용
-                .antMatchers("/css/**").permitAll()
-                // 회원 관리 처리 API 전부를 login 없이 허용
-                .antMatchers("/user/**").permitAll()
-                // 그 외 어떤 요청이든 '인증'
-                .anyRequest().authenticated()
-                .and()
-                // [로그인 기능]
-                .formLogin()
-                // 로그인 View 제공 (GET /user/login)
-                .loginPage("/user/login")
-                // 로그인 처리 (POST /user/login)
-                .loginProcessingUrl("/user/login")
-                // 로그인 처리 후 성공 시 URL
-                .defaultSuccessUrl("/")
-                // 로그인 처리 후 실패 시 URL
-                .failureUrl("/user/login?error")
-                .permitAll()
-                .and()
-                // [로그아웃 기능]
-                .logout()
-                // 로그아웃 처리 URL
-                .logoutUrl("/user/logout")
-                .permitAll();
     }
+
 }
--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -1,24 +1,25 @@
-//package com.example.demo.service;
-//
-//
-//import com.example.demo.dto.SignUpRequestDto;
-//import com.example.demo.model.User;
-//import com.example.demo.repository.UserRepository;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.Optional;
-//
-//class UserServiceTest {
-//
-//    UserService userService;
-//    UserRepository userRepository;
-//
-//
-//    @Test
-//    void registerUser() {
-//        //given
-//        Long id = 1L;
+package com.example.demo.service;
+
+
+import com.example.demo.dto.SignUpRequestDto;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+class UserServiceTest {
+
+    UserService userService;
+    UserRepository userRepository;
+
+
+    @Test
+    @DisplayName("회원가입")
+    void registerUser(){
+        //given
 //        String username = "userA";
 //        String password = "123456789";
 //        String nickname = "nickName1";
@@ -30,10 +31,14 @@
 //        //when
 //        userService.registerUser(signUpRequestDto);
 //        Optional<User> findUser = userRepository.findByUsername(username);
+//        Optional<User> findNickname = userRepository.findByNickname(nickname);
+//        Optional<User> findPhoneNumber = userRepository.findByPhoneNumber(phoneNumber);
 //
 //        //then
 //        Assertions.assertThat(signUpRequestDto).isEqualTo(findUser);
-//
-//
-//    }
-//}
+//        Assertions.assertThat(signUpRequestDto).isEqualTo(findNickname);
+//        Assertions.assertThat(signUpRequestDto).isEqualTo(findPhoneNumber);
+
+
+    }
+}
