184e39dc085dfbd418175453249dca9d1d81bd06
taehyun9244 <namtaehyun9244@gmail.com>
2022-04-22 06:45:18 +0900

    회원가입 테스트 코드 리팩토링 완성

--- src/test/java/com/example/demo/service/UserServiceTest.java
+++ src/test/java/com/example/demo/service/UserServiceTest.java
@@ -11,19 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 class UserServiceTest {
 
     @InjectMocks
@@ -79,7 +74,6 @@ class UserServiceTest {
     void registerUserTest(){
         SignUpRequestDto signUpRequestDto = new SignUpRequestDto("username", mockPasswordEncoder.encode("1234"),
                 "19920404", "namtaehyun@naver.com", "010-1111-1111", "seocho");
-        given(mockUserRepository.findById(any())).willReturn(Optional.empty());
         given(mockUserRepository.save(argThat(User -> User.getUsername().equals("username"))))
                 .willReturn(new User(signUpRequestDto.getUsername(), signUpRequestDto.getPassword(), signUpRequestDto.getBirthday()
                         , signUpRequestDto.getEmail(), signUpRequestDto.getPhoneNumber(), signUpRequestDto.getAddress()));
